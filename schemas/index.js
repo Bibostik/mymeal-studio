d042bf0357fe78e9edcb2c269a7df422846bb03a
bibostik <t.fayanjuola@rgu.ac.uk>
2023-08-01 13:23:05 +0100

    Registration and login

--- schemas/index.js
+++ schemas/index.js
@@ -2,6 +2,6 @@ import foodItem from './foodItem';
 import category from './category';
 import restaurant from './restaurant';
 import user from './user.js';
-import login from './lpogin.js'
+import login from './login.js'
 
 export const schemaTypes = [category, foodItem, restaurant, user, login]
