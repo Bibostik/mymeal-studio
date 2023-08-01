3ff95518e9a16a96ea526a3da6515d50781fe3ca
bibostik <t.fayanjuola@rgu.ac.uk>
2023-08-01 04:52:36 +0100

    my meal studio

---
+++ schemas/foodItem.js
@@ -0,0 +1,46 @@
+// schemas/schema.js
+
+export default {
+  name: 'foodItem',
+  title: 'Food Item',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: [{ type: 'category' }],
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    
+    
+  ],
+
+  
+};
+
+
---
+++ schemas/index.js
@@ -0,0 +1,5 @@
+import foodItem from './foodItem'; 
+import category from './category';
+import restaurant from './restaurant';
+
+export const schemaTypes = [category, foodItem, restaurant]
