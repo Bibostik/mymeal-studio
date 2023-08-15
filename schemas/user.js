81479ec0106d4bc3b4791d28a00b5b4311935988
BIBOSTIK <T.FAYANJUOLA@RGU.AC.UK>
2023-08-15 11:14:48 +0100

    Order fucntions

--- schemas/user.js
+++ schemas/user.js
@@ -1,29 +1,29 @@
+
+
 // schemas/user.js
 
 export default {
-    name: 'user',
-    title: 'User',
-    type: 'document',
-    fields: [
-      {
-        name: 'username',
-        title: 'Username',
-        type: 'string',
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        name: 'email',
-        title: 'Email',
-        type: 'string',
-        validation: (Rule) => Rule.required().email(),
-      },
-      {
-        name: 'password',
-        title: 'Password',
-        type: 'string',
-        validation: (Rule) => Rule.required(),
-      },
-      
-    ],
-  };
-  
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'username',
+      title: 'Username',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+      validation: (Rule) => Rule.required().email(),
+    },
+    {
+      name: 'firebaseUid',
+      title: 'Firebase UID',
+      type: 'string',
+      readOnly: true, // This field will be automatically populated and should be read-only
+    },
+  ],
+};
