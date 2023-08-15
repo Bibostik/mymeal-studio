cceeddecbda9313afda09a9792d50c787ce68e7b
BIBOSTIK <T.FAYANJUOLA@RGU.AC.UK>
2023-08-15 11:20:29 +0100

    Order fucntions

--- schemas/index.js
+++ schemas/index.js
@@ -3,5 +3,6 @@ import category from './category';
 import restaurant from './restaurant';
 import user from './user.js';
 import login from './login.js'
+import order from './order';
 
-export const schemaTypes = [category, foodItem, restaurant, user, login]
+export const schemaTypes = [category, foodItem, restaurant, user, login, order]
